59f9c57541e323f0f01917648f36a54236f64996
Santiago Viana <salviega6@gmail.com>
2024-02-18 14:28:30 -0500

    :poop: Proof

--- web/components/cnft/cnft-ui.tsx
+++ web/components/cnft/cnft-ui.tsx
@@ -1,238 +1,321 @@
-import { MetadataArgsArgs, createTree, fetchMerkleTree, fetchTreeConfigFromSeeds, transfer, mintV1, mplBubblegum, parseLeafFromMintV1Transaction, getAssetWithProof } from "@metaplex-foundation/mpl-bubblegum";
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
-import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { useMemo, useState } from "react";
-import { dasApi } from '@metaplex-foundation/digital-asset-standard-api';
-import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters';
-import { generateSigner, none, publicKey } from '@metaplex-foundation/umi';
-import { mplTokenMetadata } from "@metaplex-foundation/mpl-token-metadata";
-import toast from 'react-hot-toast';
-import { base58 } from "@metaplex-foundation/umi-serializers-encodings";
-
+import {
+	MetadataArgsArgs,
+	createTree,
+	fetchMerkleTree,
+	fetchTreeConfigFromSeeds,
+	transfer,
+	mintV1,
+	mplBubblegum,
+	parseLeafFromMintV1Transaction,
+	getAssetWithProof
+} from '@metaplex-foundation/mpl-bubblegum'
+import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
+import { useConnection, useWallet } from '@solana/wallet-adapter-react'
+import { useEffect, useMemo, useState } from 'react'
+import { dasApi } from '@metaplex-foundation/digital-asset-standard-api'
+import { walletAdapterIdentity } from '@metaplex-foundation/umi-signer-wallet-adapters'
+import { generateSigner, none, publicKey } from '@metaplex-foundation/umi'
+import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
+import toast from 'react-hot-toast'
+import { base58 } from '@metaplex-foundation/umi-serializers-encodings'
+import { firebaseApi } from '@/helpers/functions/firebase'
 // dirty way to json serialize bigint
-(BigInt.prototype as any).toJSON = function () {
-  return this.toString();
-};
+;(BigInt.prototype as any).toJSON = function () {
+	return this.toString()
+}
 
 export function CnftUi() {
-  const { connection } = useConnection()
-  const wallet = useWallet();
-  const [treeId, setTreeId] = useState<string>()
-  const [treeConfig, setTreeConfig] = useState<string>()
-  const [treeInfo, setTreeInfo] = useState<string>()
-  const [cnftId, setCnftId] = useState<string>()
-  const [cnftInfo, setCnftInfo] = useState<string>()
-  const [counter, setCounter] = useState<number>(0)
-  const [loading, setLoading] = useState<boolean>(false)
-  const [recipient, setRecipient] = useState<string>()
+	const {
+		addTicket,
+		getAllTickets,
+		getTicketByLeafId,
+		getTicketsByOwner,
+		updateTicket
+	} = firebaseApi()
+	const { connection } = useConnection()
+	const wallet = useWallet()
+	const [treeId, setTreeId] = useState<string>()
+	const [treeConfig, setTreeConfig] = useState<string>()
+	const [treeInfo, setTreeInfo] = useState<string>()
+	const [cnftId, setCnftId] = useState<string>()
+	const [cnftInfo, setCnftInfo] = useState<string>()
+	const [counter, setCounter] = useState<number>(0)
+	const [loading, setLoading] = useState<boolean>(false)
+	const [recipient, setRecipient] = useState<string>()
 
-  const umi = useMemo(() => {
-    return createUmi(connection)
-      .use(walletAdapterIdentity(wallet))
-      .use(mplTokenMetadata())
-      .use(dasApi())
-      .use(mplBubblegum())
-  }, [connection, wallet])
+	const umi = useMemo(() => {
+		return createUmi(connection)
+			.use(walletAdapterIdentity(wallet))
+			.use(mplTokenMetadata())
+			.use(dasApi())
+			.use(mplBubblegum())
+	}, [connection, wallet])
 
-  return (
-    <div className="flex gap-y-4 flex-col pb-12 min-w-full">
-      {loading && <span className="loading loading-dots loading-lg"></span>}
-      <button
-        className="btn"
-        disabled={loading}
-        onClick={async () => {
-          try {
-            setLoading(true)
-            const merkleTree = generateSigner(umi);
+	useEffect(() => {
+		;(async () => {
+			const tickets = await getAllTickets()
+			const ticketsByOwner = await getTicketsByOwner(
+				wallet.publicKey?.toBase58() || ''
+			)
+			console.log('🔥 tickets', tickets)
+			console.log('🔥🔥 ticketsByOwner', ticketsByOwner)
+		})()
+	}, [getAllTickets])
 
-            // When we create a tree at this address.
-            const builder = await createTree(umi, {
-              merkleTree,
-              maxDepth: 14,
-              maxBufferSize: 64,
-              public: false,
-            });
-            const tx = await builder.sendAndConfirm(umi);
-            const sig = base58.deserialize(tx.signature)[0]
-            setTreeId(merkleTree.publicKey)
-            console.log('created tree', sig)
-            toast.success(`Tree created, ${sig}`)
-          } catch (error: any) {
-            console.error(error);
-            toast.error(error.message)
-          } finally {
-            setLoading(false)
-          }
-        }}>Create tree</button>
+	return (
+		<div className='flex gap-y-4 flex-col pb-12 min-w-full'>
+			{loading && <span className='loading loading-dots loading-lg'></span>}
+			<button
+				className='btn'
+				disabled={loading}
+				onClick={async () => {
+					try {
+						setLoading(true)
+						const merkleTree = generateSigner(umi)
 
+						// When we create a tree at this address.
+						const builder = await createTree(umi, {
+							merkleTree,
+							maxDepth: 14,
+							maxBufferSize: 64,
+							public: false
+						})
+						const tx = await builder.sendAndConfirm(umi)
+						const sig = base58.deserialize(tx.signature)[0]
+						setTreeId(merkleTree.publicKey)
+						console.log('created tree', sig)
+						toast.success(`Tree created, ${sig}`)
+					} catch (error: any) {
+						console.error(error)
+						toast.error(error.message)
+					} finally {
+						setLoading(false)
+					}
+				}}
+			>
+				Create tree
+			</button>
 
+			<label className='form-control w-full'>
+				<div className='label'>
+					<span className='label-text'>Tree id</span>
+				</div>
+				<input
+					id='tree-id'
+					name='treeId'
+					type='text'
+					placeholder='Tree id'
+					className='input input-bordered w-full'
+					onChange={e => setTreeId(e.target.value)}
+					value={treeId}
+				/>
+			</label>
+			<button
+				type='button'
+				className='btn mt-2'
+				disabled={loading}
+				onClick={async () => {
+					if (!treeId) return
+					try {
+						setLoading(true)
+						const treeConfig = await fetchTreeConfigFromSeeds(umi, {
+							merkleTree: publicKey(treeId)
+						})
+						setTreeConfig(JSON.stringify(treeConfig, null, 2))
 
+						const treeInfo = await fetchMerkleTree(umi, publicKey(treeId))
+						setTreeInfo(JSON.stringify(treeInfo, null, 2))
+					} catch (error: any) {
+						console.error(error)
+						toast.error(error.message)
+					} finally {
+						setLoading(false)
+					}
+				}}
+			>
+				Fetch tree config
+			</button>
 
-      <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">Tree id</span>
-        </div>
-        <input id="tree-id" name="treeId" type="text" placeholder="Tree id" className="input input-bordered w-full"
-          onChange={e => setTreeId(e.target.value)}
-          value={treeId}
-        />
-      </label>
-      <button
-        type="button"
-        className="btn mt-2"
-        disabled={loading}
-        onClick={async () => {
-          if (!treeId) return;
-          try {
-            setLoading(true)
-            const treeConfig = await fetchTreeConfigFromSeeds(umi, {
-              merkleTree: publicKey(treeId),
-            });
-            setTreeConfig(JSON.stringify(treeConfig, null, 2))
+			{treeConfig && (
+				<label className='form-control w-full'>
+					<div className='label'>
+						<span className='label-text'>Tree config </span>
+					</div>
+					<textarea
+						className='textarea textarea-bordered'
+						placeholder='None'
+						value={treeConfig}
+						readOnly
+						rows={30}
+					/>
+				</label>
+			)}
 
-            const treeInfo = await fetchMerkleTree(umi, publicKey(treeId));
-            setTreeInfo(JSON.stringify(treeInfo, null, 2))
-          } catch (error: any) {
-            console.error(error);
-            toast.error(error.message)
-          } finally {
-            setLoading(false)
-          }
-        }}
-      >Fetch tree config</button>
+			{treeInfo && (
+				<label className='form-control w-full'>
+					<div className='label'>
+						<span className='label-text'>Tree info</span>
+					</div>
+					<textarea
+						className='textarea textarea-bordered'
+						placeholder='None'
+						value={treeInfo}
+						readOnly
+						rows={30}
+					/>
+				</label>
+			)}
 
+			<div className='divider' />
 
-      {treeConfig && <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">Tree config </span>
-        </div>
-        <textarea className="textarea textarea-bordered" placeholder="None" value={treeConfig} readOnly rows={30}/>
-      </label>}
+			<button
+				className='btn'
+				disabled={loading}
+				onClick={async () => {
+					if (!treeId) return
+					try {
+						setLoading(true)
+						const owner = umi.identity.publicKey
+						const metadata: MetadataArgsArgs = {
+							name: `My NFT ${counter}`,
+							uri: 'https://example.com/my-nft.json',
+							sellerFeeBasisPoints: 500, // 5%
+							collection: none(),
+							creators: []
+						}
 
-      {treeInfo && <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">Tree info</span>
-        </div>
-        <textarea className="textarea textarea-bordered" placeholder="None" value={treeInfo} readOnly rows={30}/>
-      </label>}
+						const tx = await mintV1(umi, {
+							leafOwner: owner,
+							merkleTree: publicKey(treeId),
+							metadata
+						}).sendAndConfirm(umi)
+						const sig = base58.deserialize(tx.signature)[0]
 
-      <div className="divider" />
+						const leaf = await parseLeafFromMintV1Transaction(umi, tx.signature)
 
-      <button
-        className="btn"
-        disabled={loading}
-        onClick={async () => {
-          if (!treeId) return;
-          try {
-            setLoading(true)
-            const owner = umi.identity.publicKey
-            const metadata: MetadataArgsArgs = {
-              name: `My NFT ${counter}`,
-              uri: 'https://example.com/my-nft.json',
-              sellerFeeBasisPoints: 500, // 5%
-              collection: none(),
-              creators: [],
-            };
+						setCnftId(leaf.id)
 
-            const tx = await mintV1(umi, {
-              leafOwner: owner,
-              merkleTree: publicKey(treeId),
-              metadata
-            }).sendAndConfirm(umi);
-            const sig = base58.deserialize(tx.signature)[0]
+						console.log('asset', leaf)
+						console.log('minted', sig)
 
-            const leaf = await parseLeafFromMintV1Transaction(umi, tx.signature)
+						setCounter(counter => counter + 1)
+						toast.success(`cNFT minted, ${sig}`)
+					} catch (error: any) {
+						console.error(error)
+						toast.error(error.message)
+					} finally {
+						setLoading(false)
+					}
+				}}
+			>
+				Mint cNFT
+			</button>
 
-            setCnftId(leaf.id)
+			<label className='form-control w-full'>
+				<div className='label'>
+					<span className='label-text'>cNFT id</span>
+				</div>
+				<input
+					id='cnft-id'
+					name='cnftId'
+					type='text'
+					placeholder='cNFT id'
+					className='input input-bordered w-full'
+					onChange={e => setCnftId(e.target.value)}
+					value={cnftId}
+				/>
+			</label>
+			<button
+				type='button'
+				className='btn mt-2'
+				disabled={loading}
+				onClick={async () => {
+					if (!cnftId) return
+					try {
+						setLoading(true)
+						const cnft = await umi.rpc.getAsset(publicKey(cnftId))
+						await addTicket({
+							leftId: cnft.compression.leaf_id,
+							owner: cnft.ownership.owner
+						})
 
-            console.log('asset', leaf)
-            console.log('minted', sig)
+						console.log('🔥')
+						setCnftInfo(JSON.stringify(cnft, null, 2))
+					} catch (error: any) {
+						console.error(error)
+						toast.error(error.message)
+					} finally {
+						setLoading(false)
+					}
+				}}
+			>
+				Fetch cNFT
+			</button>
 
-            setCounter((counter) => counter + 1)
-            toast.success(`cNFT minted, ${sig}`)
-          } catch (error: any) {
-            console.error(error);
-            toast.error(error.message)
-          } finally {
-            setLoading(false)
-          }
-        }}>Mint cNFT</button>
+			{cnftInfo && (
+				<label className='form-control w-full'>
+					<div className='label'>
+						<span className='label-text'>cNFT info</span>
+					</div>
+					<textarea
+						className='textarea textarea-bordered'
+						placeholder='None'
+						value={cnftInfo}
+						readOnly
+						rows={50}
+					></textarea>
+				</label>
+			)}
 
+			<label className='form-control w-full'>
+				<div className='label'>
+					<span className='label-text'>cNFT recipient</span>
+				</div>
+				<input
+					id='recipient'
+					name='recipient'
+					type='text'
+					placeholder='Recipient wallet'
+					className='input input-bordered w-full'
+					onChange={e => setRecipient(e.target.value)}
+					value={recipient}
+				/>
+			</label>
+			<button
+				type='button'
+				className='btn mt-2'
+				disabled={loading}
+				onClick={async () => {
+					if (!cnftId || !recipient) return
+					try {
+						setLoading(true)
 
-      <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">cNFT id</span>
-        </div>
-        <input id="cnft-id" name="cnftId" type="text" placeholder="cNFT id" className="input input-bordered w-full"
-          onChange={e => setCnftId(e.target.value)}
-          value={cnftId}
-        />
-      </label>
-      <button
-        type="button"
-        className="btn mt-2"
-        disabled={loading}
-        onClick={async () => {
-          if (!cnftId) return;
-          try {
-            setLoading(true)
-            const cnft = await umi.rpc.getAsset(publicKey(cnftId));
-            setCnftInfo(JSON.stringify(cnft, null, 2))
-          } catch (error: any) {
-            console.error(error);
-            toast.error(error.message)
-          } finally {
-            setLoading(false)
-          }
-        }}
-      >Fetch cNFT</button>
+						const assetWithProof = await getAssetWithProof(
+							umi,
+							publicKey(cnftId)
+						)
 
-      {cnftInfo && <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">cNFT info</span>
-        </div>
-        <textarea className="textarea textarea-bordered" placeholder="None" value={cnftInfo} readOnly rows={50}></textarea>
-      </label>}
+						const tx = await transfer(umi, {
+							...assetWithProof,
+							leafOwner: publicKey(umi.identity.publicKey),
+							newLeafOwner: publicKey(recipient)
+						}).sendAndConfirm(umi)
 
-      <label className="form-control w-full">
-        <div className="label">
-          <span className="label-text">cNFT recipient</span>
-        </div>
-        <input id="recipient" name="recipient" type="text" placeholder="Recipient wallet" className="input input-bordered w-full"
-          onChange={e => setRecipient(e.target.value)}
-          value={recipient}
-        />
-      </label>
-      <button
-        type="button"
-        className="btn mt-2"
-        disabled={loading}
-        onClick={async () => {
-          if (!cnftId || !recipient) return;
-          try {
-            setLoading(true)
+						const sig = base58.deserialize(tx.signature)[0]
 
-            const assetWithProof = await getAssetWithProof(umi, publicKey(cnftId));
+						console.log('transfer', sig)
 
-            const tx = await transfer(umi, {
-              ...assetWithProof,
-              leafOwner: publicKey(umi.identity.publicKey),
-              newLeafOwner: publicKey(recipient),
-            }).sendAndConfirm(umi);
-
-            const sig = base58.deserialize(tx.signature)[0]
-
-            console.log('transfer', sig)
-            toast.success(`cNFT transferred, ${sig}`)
-
-          } catch (error: any) {
-            console.error(error);
-            toast.error(error.message)
-          } finally {
-            setLoading(false)
-          }
-        }}
-      >Transfer cNFT</button>
-    </div>
-  )
+						toast.success(`cNFT transferred, ${sig}`)
+					} catch (error: any) {
+						console.error(error)
+						toast.error(error.message)
+					} finally {
+						setLoading(false)
+					}
+				}}
+			>
+				Transfer cNFT
+			</button>
+		</div>
+	)
 }
